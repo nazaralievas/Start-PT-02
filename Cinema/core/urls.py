67bde4b16231b822e30bd599251afc590b4485b4
nazaralievas <sadanaza4@gmail.com>
2025-07-12 16:05:18 +0600

    movie_detail added

--- Cinema/core/urls.py
+++ Cinema/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import movie_list
+from .views import *
 
 urlpatterns = [
     path('', movie_list, name='movie_name'),
+    path('movie_detail/<int:id>/', movie_detail, name='movie_detail')
 ]
--- Cinema/core/views.py
+++ Cinema/core/views.py
@@ -4,3 +4,8 @@ from .models import Movie
 def movie_list(request):
     movies = Movie.objects.all()
     return render(request, 'core/movie_list.html', {'movies': movies})
+
+
+def movie_detail(request, id):
+    movie = Movie.objects.get(id=id)
+    return render(request, 'core/movie_detail.html', {'movie': movie})
